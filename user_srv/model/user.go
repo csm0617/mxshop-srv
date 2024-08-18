ff8e9a940813dddac451ab4788ac5e4b40c67d22
‘陈水明’ <439752552@qq.com>
2024-08-18 11:22:02 +0800

    fix(model): 修改用户名为NickName

--- user_srv/model/user.go
+++ user_srv/model/user.go
@@ -17,7 +17,7 @@ type User struct {
 	BaseModel
 	Mobile   string     `gorm:"index:index_mobile;unique;type:varchar(11);not null"`
 	Password string     `gorm:"type:varchar(100);not null"` // 密码加密
-	NiceName string     `gorm:"type:varchar(20);not null"`
+	NickName string     `gorm:"type:varchar(20);not null"`
 	Birthday *time.Time `gorm:"type:datetime"`                                                        // 生日
 	Gender   string     `gorm:"column:gender;default:male;type:varchar(6) comment 'male：男，female：女'"` // "男" "女"
 	Role     int32      `gorm:"column:role;default:1;type:int(11) comment '1:普通用户，2：管理员'"`
