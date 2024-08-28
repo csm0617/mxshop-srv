f94c2bda817d27b57a31898d4d62cfb8346ad35d
‘陈水明’ <439752552@qq.com>
2024-08-29 07:35:06 +0800

    feat(config,yaml,global): 新增consul配置，mysql配置

--- user_srv/global/global.go
+++ user_srv/global/global.go
@@ -7,6 +7,6 @@ import (
 )
 
 var (
-	DB           *gorm.DB             //定义全局变量db
+	DB           *gorm.DB             = &gorm.DB{} //定义全局变量db
 	ServerConfig *config.ServerConfig = &config.ServerConfig{}
 )
